af4e9ccb9acbf61a472e98958c3f046a113654fd
jorgeclh <kolmax@gmail.com>
2018-07-16 17:04:52 +0200

    Continue post routes development

--- routes/post.js
+++ routes/post.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router  = express.Router()
 const Post = require('../models/Post')
-const {ensureLoggedIn} = require('connect-ensure-login');
+const Animal = require('../models/Animal')
+const {ensureLoggedIn} = require('connect-ensure-login')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
@@ -25,11 +26,62 @@ router.get('/detail?:id', (req, res, next) => {
 })
 
 router.get('/new', ensureLoggedIn('/auth/login'), (req, res, next) => {
-  res.render('post/new')
+  console.log(req.user._id)
+  Animal.find({userId: req.user._id})
+  .exec((err, animals) => {
+    if (err) {
+      next(err)
+      return
+    }
+
+    console.log(animals)
+
+    res.render('post/new', {animals})
+  })
 })
 
 router.post('/new', ensureLoggedIn('/auth/login'), (req, res, next) => {
-  res.render('post/list')
+  const userId = req.user._id
+  console.log(userId)
+  const animalId = req.body.animal
+  console.log(animalId)
+
+  let {description, date} = req.body
+
+  Post.findOne({animalId: animalId}, (err, foundPost) => {
+    if (err) {
+      next(err)
+      return
+    }
+
+    if (foundPost !== null) {
+      res.render('post/new', { message:'Ya existe un anuncio con esta mascota'})
+      return
+    }
+
+    const postInfo = {
+      userId,
+      animalId,
+      state: 'lost',
+      date,
+      description,
+      location: {type: 'Point', coordinates: [40,10]}
+    }
+    
+    const newPost = new Post(postInfo)
+
+    newPost.save((err) => {
+      if (err) {
+        res.render('post/new', {
+          message: 'Something went wrong. Try again later.'
+        })
+        console.log(err)
+        return
+      }
+
+      res.redirect('/post')
+    })
+  })
 })
 
 module.exports = router
