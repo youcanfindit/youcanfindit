412bc4beff520de321bd7b61f8812f88bdb5ba0c
Leismale <leismale@gmail.com>
2018-07-16 12:19:09 +0200

    User, Comments and Animal models done

---
+++ models/Animal.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const animalSchema = new Schema(
+  {
+    especie: { type: String, required: true },
+    breed: { type: String, required: true },
+    name: String,
+    color: String,
+    gender: String,
+    chip: String,
+    description: String,
+    profilePic: String,
+    username: { type: Schema.Types.ObjectId, ref: "User", required: true }
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at"
+    }
+  }
+);
+
+const Animal = mongoose.model("Animal", animalSchema);
+module.exports = Animal;
--- models/User.js
+++ models/User.js
@@ -1,15 +1,22 @@
-const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  username: String,
-  password: String
-}, {
-  timestamps: {
-    createdAt: 'created_at',
-    updatedAt: 'updated_at'
+const userSchema = new Schema(
+  {
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    name: String,
+    profilePic: String,
+    email: { type: String, required: true },
+    role: { type: String, default: true }
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at"
+    }
   }
-});
+);
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 module.exports = User;
