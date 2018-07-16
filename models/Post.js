4773b97074221608ddeba995cabc81829aa42f80
jorgeclh <kolmax@gmail.com>
2018-07-16 12:19:13 +0200

    Add Post Model

---
+++ models/Post.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+const Schema   = mongoose.Schema
+
+const postSchema = new Schema({
+  userId: {type: Schema.Types.ObjectId, ref: 'User'},
+  animalId: {type: Schema.Types.ObjectId, ref: 'Animal'},
+  state: {type: String, required: true, enum: ['found', 'lost']},
+  date: {type: Date, required: true},
+  description: {type: String, required: true},
+  location: {
+      type: {type: String},
+      coordinates: [Number]
+  }
+  reward: {type: String}
+})
+
+//Establecemos un indice de ordenación por ubicacion
+postSchema.index({location: '2dsphere'})
+//Añadimos timestamps
+postSchema.set('timestamps', true)
+
+const Post = mongoose.model('Post', postSchema)
+module.exports = Post
